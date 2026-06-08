3160b86d0a5332c1a55a3e59ef8f1525c94d06c7
liziwen <lzw20160910@sina.com>
2026-06-08 15:48:56 +0800

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 nvcc --version
+sudo apt update
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-py311_26.3.2-2-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
 
+~/miniconda3/bin/conda init bash
+source ~/.bashrc
+conda --version
+
+pip install ninja
 pip install cuda-python==13.0.0
 pip install torch --index-url https://download.pytorch.org/whl/cu130     
 
