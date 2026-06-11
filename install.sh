55f3269e41aaf3cd2d3db38f8177b7978ef190a9
liz <lzw20160910@sina.com>
2026-06-11 13:27:57 +0800

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 nvcc --version
 sudo apt update
 mkdir -p ~/miniconda3
@@ -17,12 +18,18 @@ git clone  --recursive https://github.com/NVIDIA/cutlass.git
 cd cutlass
 cd python
 export CUTLASS_NVCC_ARCHS=100
-pip install -e .
 pip install numpy
+python setup_cutlass.py develop
+# 2. 安装 cutlass 核心组件
+python setup_cutlass.py develop
+# 3. 安装 pycute 组件（NVFP4 算子开发经常需要依赖 CuTe 布局映射）
+python setup_pycute.py develop
+# 4. 安装 cutlass_library 组件
+python setup_library.py develop
 
-
-
-
+cd ~
+git clone https://github.com/liziwen-AI/NVFP4_GEMM.git
+cd NVFP4_GEMM
 
 
 
