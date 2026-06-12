552915a2e477161e12c71281e4b05c4fffa3dc46
liz <lzw20160910@sina.com>
2026-06-12 15:49:20 +0800

    3

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e  # 出错立即停止
 
+# git clone https://github.com/liziwen-AI/NVFP4_GEMM.git
 nvcc --version
 sudo apt update
 mkdir -p ~/miniconda3
@@ -9,23 +10,20 @@ bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -f ~/miniconda3/miniconda.sh
 
 export PATH=~/miniconda3/bin:$PATH
-
 pip install ninja
 pip install cuda-python==13.0.0
 pip install torch --index-url https://download.pytorch.org/whl/cu130     
 
-# pip install nvidia-cutlass-dsl[cu13]
-git clone  --recursive https://github.com/NVIDIA/cutlass.git
-export CUTLASS_NVCC_ARCHS=100
-# 安装 CUTLASS 主包
-cd cutlass
-pip install numpy
-pip install -e .
-
-
+pip install nvidia-cutlass-dsl[cu13]
+# python -c "import cutlass.cute as cute; print('OK')"
+# git clone  --recursive https://github.com/NVIDIA/cutlass.git
+# export CUTLASS_NVCC_ARCHS=100
+# # 安装 CUTLASS 主包
+# cd cutlass
+# pip install numpy
+# pip install -e .
 
 cd ~
-git clone https://github.com/liziwen-AI/NVFP4_GEMM.git
 cd NVFP4_GEMM
 python liz.py
 
--- liz.py
+++ liz.py
@@ -163,8 +163,7 @@ check_implementation = make_match_reference(ref_kernel, rtol=1e-03, atol=1e-03)
 
 m, n, k, l, seed = 128, 256, 256, 1, 1111
 data = generate_input(m, n, k, l, seed)
-a_ref, b_ref, _, _, sfa_permuted, sfb_permuted, c_ref = data
-my_output = custom_kernel(a_ref, b_ref, sfa_permuted, sfb_permuted, c_ref.clone())
+my_output = custom_kernel(data)
 is_correct, error_msg = check_implementation(data, my_output)
 if is_correct:
     print("🎉 恭喜！你的自定义算子实现与参考实现完全吻合，精度达标！")
