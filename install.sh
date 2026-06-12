4e0fd9198a2cae3570a9a8edd090d213aa19e0f0
liz <lzw20160910@sina.com>
2026-06-12 14:19:23 +0800

    1

--- install.sh
+++ install.sh
@@ -14,19 +14,20 @@ pip install ninja
 pip install cuda-python==13.0.0
 pip install torch --index-url https://download.pytorch.org/whl/cu130     
 
+# pip install nvidia-cutlass-dsl[cu13]
 git clone  --recursive https://github.com/NVIDIA/cutlass.git
-cd cutlass/python
 export CUTLASS_NVCC_ARCHS=100
+# 安装 CUTLASS 主包
+cd cutlass
 pip install numpy
-
 pip install -e .
+# 安装 CuTe DSL 包
+pip install -e .
+
+
 
 cd ~
 git clone https://github.com/liziwen-AI/NVFP4_GEMM.git
 cd NVFP4_GEMM
-
-
-
-
-
+python liz.py
 
