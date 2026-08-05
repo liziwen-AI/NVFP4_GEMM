0ba59868e49ab4291342b1c2076f0f703b8a8521
liz <lzw20160910@sina.com>
2026-08-05 15:54:21 +0800

    ...

--- liz.py
+++ liz.py
@@ -3,10 +3,10 @@ import numpy as np
 from torch.cuda.nvtx import range as nvtx_range
 from task import input_t, output_t
 from utils import make_match_reference
-# from contestant.submission import custom_kernel
+from contestant.submission import custom_kernel
 # from contestant.gau_nernst import custom_kernel
 # from contestant.CatsRCool import custom_kernel
-from contestant.s_am import custom_kernel
+# from contestant.s_am import custom_kernel
 
 # Scaling factor vector size
 sf_vec_size = 16
